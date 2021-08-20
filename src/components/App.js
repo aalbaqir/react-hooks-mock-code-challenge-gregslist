a9ae2d8817bd31fa3f154d661efa45b7dcd579ce
Aidah Al Baqir <aidah.albaqir@gmail.com>
2021-08-20 00:13:21 -0500

    commit tree ddad04b516300e9132378d6fde70ac4dbf9548a1

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [listings, setListings] = useState ([]);
+  const [headerlist, setHeaderList] = useState([]);
+
+useEffect(() =>{
+  fetch('http://example.com/movies.json')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+}, [] )
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer dataTopMap={listing}/>
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,16 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer(props) {
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {props.dataToMap.map( eachListing => {
+
+
+          return <ListingCard key={eachListing.id} listingToRender={eachStock}/>
+        
+      })}
       </ul>
     </main>
   );
